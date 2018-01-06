1f5294c812fb11b756fee5023773356d3028ec1a
Terry Zhuo <terryzhuo@skysolarenergy.com.au>
2018-01-06 16:08:55 +0000

    doctype update

--- sse/config/desktop.py
+++ sse/config/desktop.py
@@ -5,6 +5,13 @@ from frappe import _
 def get_data():
 	return [
 		{
+			"module_name": "SSE App",
+			"color": "red",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("SSE App")
+		},
+		{
 			"module_name": "Installation",
 			"color": "red",
 			"icon": "octicon octicon-file-directory",
