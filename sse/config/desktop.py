99f8eb6cdc9daa462698f04b7f3539e72a521960
Terry Zhuo <terry@Terrys-MacBook-Pro.local>
2018-01-07 02:29:58 +1100

    module update

--- sse/config/desktop.py
+++ sse/config/desktop.py
@@ -5,10 +5,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "SSE App",
+			"module_name": "Installation",
 			"color": "red",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("SSE App")
+			"label": _("Installation")
 		}
 	]
